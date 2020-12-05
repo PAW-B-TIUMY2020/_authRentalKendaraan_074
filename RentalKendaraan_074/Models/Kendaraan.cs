4dfde02e079123bb2c21d6fcc251885cb7f82de0
eeRha <arrasyidtz3@gmail.com>
2020-12-05 12:28:59 +0700

    Add project files.

---
+++ RentalKendaraan_074/Models/Kendaraan.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace RentalKendaraan_074.Models
+{
+    public partial class Kendaraan
+    {
+        public Kendaraan()
+        {
+            Peminjaman = new HashSet<Peminjaman>();
+        }
+
+        public int IdKendaraan { get; set; }
+        [Required(ErrorMessage = "Nama kendaraan tidak boleh kosong!")]
+        public string NamaKendaraan { get; set; }
+        [Required(ErrorMessage = "No Polisi tidak boleh kosong!")]
+        public string NoPolisi { get; set; }
+        [Required(ErrorMessage = "No STNK tidak boleh kosong!")]
+        public string NoStnk { get; set; }
+        public int? IdJenisKendaraan { get; set; }
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Hanya boleh diisi angka!")]
+        public string Ketersediaan { get; set; }
+
+        public JenisKendaraan IdJenisKendaraanNavigation { get; set; }
+        public ICollection<Peminjaman> Peminjaman { get; set; }
+    }
+}
---
+++ RentalKendaraan_074/Models/Peminjaman.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace RentalKendaraan_074.Models
+{
+    public partial class Peminjaman
+    {
+        public Peminjaman()
+        {
+            Pengembalian = new HashSet<Pengembalian>();
+        }
+        [DisplayName("ID")]
+        public int IdPeminjaman { get; set; }
+        [Required(ErrorMessage = "Tanggal peminjaman tidak boleh kosong!")]
+        public DateTime? TglPeminjaman { get; set; }
+        [DisplayName("Kendaraan")]
+        [Required(ErrorMessage = "Nama kendaraan tidak boleh kosong!")]
+        public int? IdKendaraan { get; set; }
+        [DisplayName("Costumer")]
+        [Required(ErrorMessage = "Nama cutomer tidak boleh kosong!")]
+        public int? IdCostumer { get; set; }
+        [DisplayName("Jaminan")]
+        [Required(ErrorMessage = "Jaminan tidak boleh kosong!")]
+        public int? IdJaminan { get; set; }
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Hanya boleh diisi dengan angka")]
+        public int? Biaya { get; set; }
+
+        [DisplayName("Customer")]
+        public Customer IdCostumerNavigation { get; set; }
+        [DisplayName("Jaminan")]
+        public Jaminan IdJaminanNavigation { get; set; }
+        [DisplayName("Kendaraan")]
+        public Kendaraan IdKendaraanNavigation { get; set; }
+        public ICollection<Pengembalian> Pengembalian { get; set; }
+    }
+}
